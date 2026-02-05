5d895788d24fc05ce6b0aea20ab424105e11654a
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-02-05 11:21:12 +0700

    Add project files.

---
+++ backend/services/config-server/src/test/java/vn/edu/uit/msshop/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.uit.msshop.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
