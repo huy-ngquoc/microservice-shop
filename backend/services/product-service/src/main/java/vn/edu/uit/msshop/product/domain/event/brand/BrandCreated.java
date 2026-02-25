2f8c4cf09b221c2bdf8384b4512a3447c5f0e75b
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-02-26 01:47:19 +0700

    Add domain event in "product-service".

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/domain/event/brand/BrandCreated.java
@@ -0,0 +1,22 @@
+package vn.edu.uit.msshop.product.domain.event.brand;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.domain.model.brand.valueobject.BrandId;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(
+        onlyExplicitlyIncluded = true)
+public class BrandCreated {
+    @EqualsAndHashCode.Include
+    private final UUID eventId = UUID.randomUUID();
+
+    private final Instant occurrenceTime = Instant.now();
+
+    private final BrandId brandId;
+}
---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/domain/event/category/CategoryUpdated.java
@@ -0,0 +1,22 @@
+package vn.edu.uit.msshop.product.domain.event.category;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.domain.model.category.valueobject.CategoryId;
+
+@RequiredArgsConstructor
+@Getter
+@EqualsAndHashCode(
+        onlyExplicitlyIncluded = true)
+public class CategoryUpdated {
+    @EqualsAndHashCode.Include
+    private final UUID eventId = UUID.randomUUID();
+
+    private final Instant occurrenceTime = Instant.now();
+
+    private final CategoryId categoryId;
+}
