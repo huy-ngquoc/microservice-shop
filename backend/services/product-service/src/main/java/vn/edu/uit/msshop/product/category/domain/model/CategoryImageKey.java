2d023d55848863f4203f5ac49c9f485320a37253
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-03-15 16:33:48 +0700

    Add category image key validation.

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/domain/model/CategoryImageKey.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/domain/model/CategoryImageKey.java
@@ -4,15 +4,21 @@ import vn.edu.uit.msshop.product.shared.domain.exception.DomainException;
 
 public record CategoryImageKey(
         String value) {
+    public static final int MAX_LENGTH_VALUE = 255;
+
     public CategoryImageKey {
         if (value == null) {
             throw new DomainException("Key is null");
         }
 
-        value = value.trim();
+        if (value.length() > MAX_LENGTH_VALUE) {
+            throw new DomainException("Key is too long");
+        }
 
         if (value.isBlank()) {
             throw new DomainException("Key is blank");
         }
+
+        value = value.trim();
     }
 }
