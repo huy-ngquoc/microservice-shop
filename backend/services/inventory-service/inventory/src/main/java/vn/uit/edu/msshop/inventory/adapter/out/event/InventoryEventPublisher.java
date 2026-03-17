7c40d4cc0349f8eeec2cc9305ba20e46f3db0232
BinhUIT <22520128@gm.uit.edu.vn>
2026-03-17 18:39:58 +0700

    Them su kien update nhieu variant cung luc

--- backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/adapter/out/event/InventoryEventPublisher.java
+++ backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/adapter/out/event/InventoryEventPublisher.java
@@ -10,12 +10,14 @@ import lombok.RequiredArgsConstructor;
 import vn.uit.edu.msshop.inventory.application.port.out.PublishInventoryEventPort;
 import vn.uit.edu.msshop.inventory.domain.event.ForceCancellOrder;
 import vn.uit.edu.msshop.inventory.domain.event.InventoryUpdated;
+import vn.uit.edu.msshop.inventory.domain.event.UpdateManyInventoriesEvent;
 
 @Component
 @RequiredArgsConstructor
 public class InventoryEventPublisher implements PublishInventoryEventPort {
     private final KafkaTemplate<String, InventoryUpdated> inventoryUpdateTemplate;
     private final KafkaTemplate<String, ForceCancellOrder> forceCancellOrderTemplate;
+    private final KafkaTemplate<String, UpdateManyInventoriesEvent> updateManyTemplate;
     private static final String INVENTORY_TOPIC="";
     private static final String ORDER_TOPIC="";
 
@@ -29,6 +31,12 @@ public class InventoryEventPublisher implements PublishInventoryEventPort {
     public void publishForceCancellOrderEvent(ForceCancellOrder event) {
         Message<ForceCancellOrder> message= MessageBuilder.withPayload(event).setHeader(KafkaHeaders.TOPIC, ORDER_TOPIC).build();
         forceCancellOrderTemplate.send(message);
+    }
+
+    @Override
+    public void publicUpdateManyInventoriesEvent(UpdateManyInventoriesEvent event) {
+        Message<UpdateManyInventoriesEvent> message= MessageBuilder.withPayload(event).setHeader(KafkaHeaders.TOPIC, ORDER_TOPIC).build();
+        updateManyTemplate.send(message);
     }
 
 }
