39f4f5c5516f48e3fb458afd802a08d92f523037
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-03-26 20:24:32 +0700

    Add Cloudinary configuration and implement VariantImageStorageAdapter for image management.

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/bootstrap/config/CloudinaryConfig.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/bootstrap/config/CloudinaryConfig.java
@@ -1,5 +1,6 @@
 package vn.edu.uit.msshop.product.bootstrap.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,15 +9,16 @@ import com.cloudinary.Cloudinary;
 import vn.edu.uit.msshop.product.bootstrap.config.properties.CloudinaryProperties;
 
 @Configuration
+@EnableConfigurationProperties(CloudinaryProperties.class)
 public class CloudinaryConfig {
     @Bean
     Cloudinary cloudinary(
             CloudinaryProperties properties) {
         final var url = String.format(
                 "cloudinary://%s:%s@%s",
+                properties.cloudName(),
                 properties.apiKey(),
-                properties.apiSecret(),
-                properties.cloudName());
+                properties.apiSecret());
 
         return new Cloudinary(url);
     }
