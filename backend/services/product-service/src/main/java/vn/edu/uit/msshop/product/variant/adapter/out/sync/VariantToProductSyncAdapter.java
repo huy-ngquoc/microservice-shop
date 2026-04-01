07c61bbc224317b0077e6e073a2db6937db31748
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-04-01 11:24:50 +0700

    Implement methods in VariantToProductSyncAdapter.

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/out/sync/VariantToProductSyncAdapter.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/out/sync/VariantToProductSyncAdapter.java
@@ -3,6 +3,15 @@ package vn.edu.uit.msshop.product.variant.adapter.out.sync;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.product.application.dto.command.RemoveProductVariantForVariantCommand;
+import vn.edu.uit.msshop.product.product.application.dto.command.UpdateProductVariantForVariantCommand;
+import vn.edu.uit.msshop.product.product.application.port.in.command.RemoveProductVariantForVariantUseCase;
+import vn.edu.uit.msshop.product.product.application.port.in.command.UpdateProductVariantForVariantUseCase;
+import vn.edu.uit.msshop.product.product.domain.model.ProductVariant;
+import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductId;
+import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductVariantId;
+import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductVariantPrice;
+import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductVariantTraits;
 import vn.edu.uit.msshop.product.variant.application.port.out.sync.RemoveVariantFromProductPort;
 import vn.edu.uit.msshop.product.variant.application.port.out.sync.UpdateVariantInProductPort;
 import vn.edu.uit.msshop.product.variant.domain.model.Variant;
@@ -14,18 +23,40 @@ import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantProduct
 public class VariantToProductSyncAdapter
         implements UpdateVariantInProductPort,
         RemoveVariantFromProductPort {
+    private final UpdateProductVariantForVariantUseCase updateProductVariantForVariantUseCase;
+    private final RemoveProductVariantForVariantUseCase removeProductVariantForVariantUseCase;
 
     @Override
     public void updateInProduct(
             final Variant variant) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateInProduct'");
+        final var productVariantId = new ProductVariantId(variant.getId().value());
+        final var productVariantPrice = new ProductVariantPrice(variant.getPrice().value());
+        final var productVariantTraits = ProductVariantTraits.of(variant.getTraits().unwrap());
+
+        final var productVariant = new ProductVariant(
+                productVariantId,
+                productVariantPrice,
+                productVariantTraits);
+        final var productId = new ProductId(variant.getProductId().value());
+
+        final var command = new UpdateProductVariantForVariantCommand(
+                productId,
+                productVariant);
+
+        this.updateProductVariantForVariantUseCase.updateVariant(command);
     }
 
     @Override
     public void removeFromProduct(
-            final VariantId id,
-            final VariantProductId productId) {
+            final VariantId variantId,
+            final VariantProductId variantProductId) {
+        final var productVariantId = new ProductVariantId(variantId.value());
+        final var productId = new ProductId(variantProductId.value());
 
+        final var command = new RemoveProductVariantForVariantCommand(
+                productId,
+                productVariantId);
+
+        this.removeProductVariantForVariantUseCase.removeVariant(command);
     }
 }
