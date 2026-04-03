c11732175ba6299b814d4231b1cc2f6e516bc31d
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-04-03 15:41:55 +0700

    Fix validation annotations in request classes to use @NotNull instead of @NotBlank for price and version fields

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/adapter/in/web/request/AddProductVariantRequest.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/adapter/in/web/request/AddProductVariantRequest.java
@@ -10,7 +10,7 @@ import jakarta.validation.constraints.Size;
 import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductVariantTrait;
 
 public record AddProductVariantRequest(
-        @NotBlank
+        @NotNull
         @PositiveOrZero
         Long price,
 
