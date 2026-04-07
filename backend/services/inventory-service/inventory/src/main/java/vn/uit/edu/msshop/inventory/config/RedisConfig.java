35b4b4f1249acdd6b7875eee2ee17d118a9014b9
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-07 15:17:14 +0700

    Them redis lam cache

--- backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/config/RedisConfig.java
+++ backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/config/RedisConfig.java
@@ -11,13 +11,54 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import lombok.Getter;
+
 @Configuration
+@Getter
 public class RedisConfig {
-    @Bean
+    private DefaultRedisScript<Long> reserveStockScript;
+    private DefaultRedisScript<Long> reserveShippingStockScript;
+    private DefaultRedisScript<Long> cancelStockScript;
+    private DefaultRedisScript<Long> releaseStockScript;
+
+    public RedisConfig() {
+        reserveStockScript=  reserveStockScript();
+        reserveShippingStockScript= reserveShippingStockScript();
+        cancelStockScript = cancelStockScript();
+        releaseStockScript=releaseStockScript();
+    }
+    
+    
     public DefaultRedisScript<Long> reserveStockScript() {
         DefaultRedisScript<Long> script = new DefaultRedisScript<>();
         
         script.setLocation(new ClassPathResource("reserve_stock.lua"));
+        
+        script.setResultType(Long.class);
+        return script;
+    }
+    
+    public DefaultRedisScript<Long> cancelStockScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        
+        script.setLocation(new ClassPathResource("cancel_stock.lua"));
+        
+        script.setResultType(Long.class);
+        return script;
+    } 
+
+    public DefaultRedisScript<Long> releaseStockScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        
+        script.setLocation(new ClassPathResource("release_stock.lua"));
+        
+        script.setResultType(Long.class);
+        return script;
+    }
+    public DefaultRedisScript<Long> reserveShippingStockScript() {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        
+        script.setLocation(new ClassPathResource("reverse_ship.lua"));
         
         script.setResultType(Long.class);
         return script;
