1a3d0c96268747e4e1f4210310e028e3bc592e60
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-10 09:11:28 +0700

    Sua loi nullable

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/in/web/VariantInventoryEventListener.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/in/web/VariantInventoryEventListener.java
@@ -13,25 +13,30 @@ import vn.edu.uit.msshop.product.shared.event.EventDocumentRepository;
 import vn.edu.uit.msshop.product.variant.application.port.out.persistence.LoadVariantPort;
 import vn.edu.uit.msshop.product.variant.application.port.out.persistence.SaveVariantPort;
 import vn.edu.uit.msshop.product.variant.domain.event.InventoryUpdated;
-import vn.edu.uit.msshop.product.variant.domain.model.Variant;
 import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantId;
 import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantStock;
 
-@KafkaListener(topics="inventory-product")
+@KafkaListener(
+        topics = "inventory-product")
 @Component
 @RequiredArgsConstructor
 public class VariantInventoryEventListener {
     private final LoadVariantPort loadVariantPort;
     private final EventDocumentRepository eventDocumentRepo;
     private final SaveVariantPort saveVariantPort;
-    
+
     @KafkaHandler
     @Transactional
-    public void onInventoryUpdate(InventoryUpdated event) {
-        if(eventDocumentRepo.existsById(event.getEventId())) return;
-        Variant variant = loadVariantPort.loadById(new VariantId(event.getVariantId())).orElse(null);
-        final var toSave = variant.updateStock(new VariantStock(event.getNewQuantity()));
-        saveVariantPort.save(toSave);
-        eventDocumentRepo.save(new EventDocument(event.getEventId(),Instant.now()));
+    public void onInventoryUpdate(
+            InventoryUpdated event) {
+        if (eventDocumentRepo.existsById(event.getEventId()))
+            return;
+        loadVariantPort.loadById(new VariantId(event.getVariantId()))
+                .ifPresent(variant -> {
+                    var toSave = variant.updateStock(new VariantStock(event.getNewQuantity()));
+                    saveVariantPort.save(toSave);
+                    eventDocumentRepo.save(new EventDocument(event.getEventId(), Instant.now()));
+                });
+
     }
 }
