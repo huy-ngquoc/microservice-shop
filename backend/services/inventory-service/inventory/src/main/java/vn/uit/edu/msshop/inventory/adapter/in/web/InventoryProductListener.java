9013c99ed66095359670594175b90b80267e41a9
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-14 16:42:10 +0700

    Them lang nghe su kien tao product

--- backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/adapter/in/web/InventoryProductListener.java
+++ backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/adapter/in/web/InventoryProductListener.java
@@ -1,40 +1,24 @@
 package vn.uit.edu.msshop.inventory.adapter.in.web;
 
-import java.time.Instant;
+import java.util.List;
 
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import vn.uit.edu.msshop.inventory.adapter.out.event.documents.EventDocument;
-import vn.uit.edu.msshop.inventory.adapter.out.event.repositories.EventDocumentRepository;
 import vn.uit.edu.msshop.inventory.application.port.in.CreateInventoryUseCase;
-import vn.uit.edu.msshop.inventory.application.port.in.DeleteInventoryUseCase;
-import vn.uit.edu.msshop.inventory.domain.event.VariantCreated;
-import vn.uit.edu.msshop.inventory.domain.event.VariantDeleted;
+import vn.uit.edu.msshop.inventory.domain.event.product.ProductCreated;
 import vn.uit.edu.msshop.inventory.domain.model.valueobject.VariantId;
 
-@RestController
+@Component
 @RequiredArgsConstructor
-@KafkaListener(topics="product-inventory",groupId="product-group")
+@KafkaListener(topics="product-topic", groupId="product-group")
 public class InventoryProductListener {
     private final CreateInventoryUseCase createUseCase;
-    private final DeleteInventoryUseCase deleteUseCase;
-    private final EventDocumentRepository eventDocumentRepo;
-    
     @KafkaHandler
-    public void onVariantCreated(VariantCreated event) {
-        if(eventDocumentRepo.existsById(event.getEventId())) return;
-        createUseCase.create(new VariantId(event.getVariantId()));
-        eventDocumentRepo.save(EventDocument.builder().eventId(event.getEventId()).receiveAt(Instant.now()).build());
+    public void onProductCreated(ProductCreated productCreated) {
+        List<VariantId> variantIds = productCreated.getVariantCreateds().stream().map(item->new VariantId(item.getVariantId())).toList();
+        createUseCase.createNewsFromListVariantId(variantIds);
     }
-
-    @KafkaHandler
-    public void onVariantDeleted(VariantDeleted event) {
-        if(eventDocumentRepo.existsById(event.getEventId())) return;
-        deleteUseCase.deleteByVariantId(new VariantId(event.getVariantId()));
-        eventDocumentRepo.save(EventDocument.builder().eventId(event.getEventId()).receiveAt(Instant.now()).build());
-    }
-
 }
