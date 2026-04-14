1dc9a87da27c296a88c5f9e2ddc04a14b96fb490
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-14 09:53:00 +0700

    Them event publisher

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/shared/application/port/out/PublishProductEventPort.java
@@ -0,0 +1,15 @@
+package vn.edu.uit.msshop.product.shared.application.port.out;
+
+import vn.edu.uit.msshop.product.shared.event.document.ProductCreatedDocument;
+import vn.edu.uit.msshop.product.shared.event.document.ProductDeletedDocument;
+import vn.edu.uit.msshop.product.shared.event.document.ProductUpdateDocument;
+import vn.edu.uit.msshop.product.shared.event.document.VariantDeletedDocument;
+import vn.edu.uit.msshop.product.shared.event.document.VariantUpdateDocument;
+
+public interface PublishProductEventPort {
+    public void publishProductCreated(ProductCreatedDocument eventDocument);
+    public void publishProductUpdated(ProductUpdateDocument eventDocument);
+    public void publishVariantUpdated(VariantUpdateDocument eventDocument);
+    public void publishVariantDeleted(VariantDeletedDocument eventDocument);
+    public void publishProductDeleted(ProductDeletedDocument eventDocument);
+}
