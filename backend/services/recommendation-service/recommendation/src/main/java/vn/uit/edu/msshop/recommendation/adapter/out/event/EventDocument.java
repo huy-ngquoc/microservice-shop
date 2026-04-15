44414e9e50dd1820afff4fbe41fddbfcab83201b
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-15 08:19:48 +0700

    sua event document

--- backend/services/recommendation-service/recommendation/src/main/java/vn/uit/edu/msshop/recommendation/adapter/out/event/EventDocument.java
+++ backend/services/recommendation-service/recommendation/src/main/java/vn/uit/edu/msshop/recommendation/adapter/out/event/EventDocument.java
@@ -8,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Document
