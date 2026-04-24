9fcde3cc7dabdc4c56aee19833f30bb06dc40ce5
BinhUIT <22520128@gm.uit.edu.vn>
2026-04-24 07:22:40 +0700

    Xoa cac ham ko dung

--- backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/application/port/in/UpdateInventoryUseCase.java
+++ backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/application/port/in/UpdateInventoryUseCase.java
@@ -3,14 +3,13 @@ package vn.uit.edu.msshop.inventory.application.port.in;
 import java.util.List;
 
 import vn.uit.edu.msshop.inventory.application.dto.command.OrderCancelledCommand;
-import vn.uit.edu.msshop.inventory.application.dto.command.OrderCreateCommand;
 import vn.uit.edu.msshop.inventory.application.dto.command.OrderShippedCommand;
 import vn.uit.edu.msshop.inventory.application.dto.command.UpdateInventoryCommand;
 import vn.uit.edu.msshop.inventory.application.dto.query.InventoryView;
 
 public interface UpdateInventoryUseCase {
     public InventoryView update(UpdateInventoryCommand command);
-    public List<InventoryView> updateWhenOrderCreated(OrderCreateCommand command);
+    
     public List<InventoryView> updateWhenOrderCancelled(OrderCancelledCommand command);
     public List<InventoryView> updateWhenOrderShipped(OrderShippedCommand command);
 }
--- backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/application/service/UpdateInventoryService.java
+++ backend/services/inventory-service/inventory/src/main/java/vn/uit/edu/msshop/inventory/application/service/UpdateInventoryService.java
@@ -14,17 +14,14 @@ import org.springframework.transaction.support.TransactionSynchronization;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import lombok.RequiredArgsConstructor;
-import vn.uit.edu.msshop.inventory.adapter.out.event.documents.ForceCancellOrderDocument;
 import vn.uit.edu.msshop.inventory.adapter.out.event.documents.InventoryUpdatedDocument;
 import vn.uit.edu.msshop.inventory.adapter.out.event.repositories.ForceCancellOrderDocumentRepository;
 import vn.uit.edu.msshop.inventory.adapter.out.event.repositories.InventoryUpdatedDocumentRepository;
 import vn.uit.edu.msshop.inventory.application.dto.command.OrderCancelledCommand;
-import vn.uit.edu.msshop.inventory.application.dto.command.OrderCreateCommand;
 import vn.uit.edu.msshop.inventory.application.dto.command.OrderDetailCommand;
 import vn.uit.edu.msshop.inventory.application.dto.command.OrderShippedCommand;
 import vn.uit.edu.msshop.inventory.application.dto.command.UpdateInventoryCommand;
 import vn.uit.edu.msshop.inventory.application.dto.query.InventoryView;
-import vn.uit.edu.msshop.inventory.application.exception.InsufficientStockException;
 import vn.uit.edu.msshop.inventory.application.exception.InventoryNotFoundException;
 import vn.uit.edu.msshop.inventory.application.mapper.InventoryViewMapper;
 import vn.uit.edu.msshop.inventory.application.port.in.UpdateInventoryUseCase;
@@ -84,89 +81,11 @@ public class UpdateInventoryService implements UpdateInventoryUseCase {
         return mapper.toView(saved);
     }
 
-    @Override
-    @org.springframework.transaction.annotation.Transactional
-    public List<InventoryView> updateWhenOrderCreated(OrderCreateCommand commands) {
-        List<Inventory> inventories = loadFromRedisPort.loadFromRedis(commands.getDetailCommands().stream().map(item->item.getVariantId()).toList());
-        //System.out.println("Inventories size "+inventories.get(0).getVariantId().value().toString() );
-        List<Inventory> toSaves = new ArrayList<>();
-        List<InventoryUpdatedDocument> events = new ArrayList<>();
-        for(OrderDetailCommand detailCommand: commands.getDetailCommands()) {
-            //System.out.println(detailCommand.getVariantId().value());
-            Inventory inventory = findByVariantIdInList(detailCommand.getVariantId(), inventories);
-            if(inventory==null) throw new RuntimeException("Invalid variant id");
-            if(inventory.getQuantity().value()<detailCommand.getQuantity().value()) {
-
-                //publishEventPort.publishForceCancellOrderEvent(new ForceCancellOrder(commands.getOrderId()));
-                final var outboxEvent = forceCancellOrderDocumentRepo.save(ForceCancellOrderDocument.builder().orderId(commands.getOrderId()).eventId(UUID.randomUUID()).build());
-                publishEventPort.publishForceCancellOrderEvent(outboxEvent);
-                return null;
-            }
-            int newQuantity = inventory.getQuantity().value()-detailCommand.getQuantity().value();
-            int newReservedQuantity = inventory.getReservedQuantity().value()+detailCommand.getQuantity().value();
-            final var updateInfo = Inventory.UpdateInfo.builder().inventoryId(inventory.getId()).quantity(new Quantity(newQuantity)).reservedQuantity(new ReservedQuantity(newReservedQuantity)).build();
-            final var toSave = inventory.applyUpdateInfo(updateInfo);
-            toSaves.add(toSave);
-            InventoryUpdatedDocument event = InventoryUpdatedDocument.builder().eventId(UUID.randomUUID())
-        .variantId(toSave.getVariantId().value())
-        .newQuantity(toSave.getQuantity().value())
-        .newReservedQuantity(toSave.getReservedQuantity().value())
-        .eventStatus("PENDING")
-        .retryCount(0)
-        .createdAt(Instant.now())
-        .updatedAt(null)
-        .lastError(null)
-        .isRead(false)
-        .build();
-            events.add(event);
-            
-        }
-                
-            
-        
-        //publishEventPort.publicUpdateManyInventoriesEvent(new UpdateManyInventoriesEvent(events));
-        
-        for(OrderDetailCommand command: commands.getDetailCommands()) {
-            //System.out.println("Call check and reserve, amount "+command.getQuantity().value());
-            processScript(command.getVariantId().value(), command.getQuantity().value(), redisConfig.getReserveStockScript());
-        }
-        inventoryUpdatedDocumentRepo.saveAll(events);
-         TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
-            @Override
-            public void afterCommit() {
-                for(final var event:events) {
-                    publishEventPort.publishInventoryUpdateEvent(event);
-                }
-            }
-        });
-        
-        return toSaves.stream().map(mapper::toView).toList();
     
-    
-    }
 
     
 
-    public void processScript(UUID variantId, int amount, DefaultRedisScript<Long> script) {
-        String key = "inventory:variant:" + variantId.toString();
-    
-    Long result = redisTemplate.execute(script, List.of(key), String.valueOf(amount));
-
-    if (result == -1) {
-        // Tình huống: Redis trống trơn (hết hạn hoặc chưa nạp)
-        //System.out.println("Redis miss! Loading from DB...");
-        Inventory inventory = syncPort.loadFromMainDatabase(variantId);
-        if(inventory==null) throw new InventoryNotFoundException(new VariantId(variantId));
-        // Sau khi load xong thì gọi lại chính nó (Recursive) hoặc báo user thử lại
-        processScript(variantId, amount, script); 
-    } else if (result == 0) {
-        throw new InsufficientStockException(new VariantId(variantId));
-    } else if (result == -2) {
-        throw new IllegalStateException("Dữ liệu tồn kho trên Redis bị lỗi định dạng!");
-    } else if (result == 1) {
-        
-    }
-    }
+   
 
     
 
@@ -303,22 +222,7 @@ public class UpdateInventoryService implements UpdateInventoryUseCase {
     
     }
 
-    private InventoryUpdatedDocument createUpdateEvent(Inventory inventory) {
-    return InventoryUpdatedDocument.builder()
-        .eventId(UUID.randomUUID())
-        .variantId(inventory.getVariantId().value())
-        .newQuantity(inventory.getQuantity().value())
-        .newReservedQuantity(inventory.getReservedQuantity().value())
-        .eventStatus("PENDING")
-        .createdAt(Instant.now())
-        .build();
-}
-private void handleForceCancel(UUID orderId) {
-    var cancelEvent = ForceCancellOrderDocument.builder()
-            .orderId(orderId).eventId(UUID.randomUUID()).build();
-    forceCancellOrderDocumentRepo.save(cancelEvent);
-    publishEventPort.publishForceCancellOrderEvent(cancelEvent);
-}
+    
 public void processOrderDetail(List<OrderDetail> orderDetails, DefaultRedisScript<Long> script) {
         
         List<String> keys = orderDetails.stream()
