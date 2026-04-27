68ab3f4518ca718ecd72c231cb1e94eeae6d4939
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-04-27 18:42:00 +0700

    Remove CreateVariantSoldCountPort interface as it's not used.

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/application/port/out/persistence/CreateVariantSoldCountPort.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.uit.msshop.product.variant.application.port.out.persistence;
-
-import vn.edu.uit.msshop.product.variant.domain.model.VariantSoldCount;
-import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantId;
-
-public interface CreateVariantSoldCountPort {
-    VariantSoldCount create(
-            final VariantId id);
-}
