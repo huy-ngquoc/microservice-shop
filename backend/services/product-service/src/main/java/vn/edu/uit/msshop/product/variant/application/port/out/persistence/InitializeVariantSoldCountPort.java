f4534d705f9692ad9c6547f59e3d74b1b28a3d49
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-04-27 18:42:44 +0700

    Refactor InitializeVariantSoldCountPort to accept NewVariantSoldCount parameter and update VariantSoldCountPersistenceAdapter accordingly

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/application/port/out/persistence/InitializeVariantSoldCountPort.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/application/port/out/persistence/InitializeVariantSoldCountPort.java
@@ -1,11 +1,9 @@
 package vn.edu.uit.msshop.product.variant.application.port.out.persistence;
 
 import vn.edu.uit.msshop.product.variant.domain.model.VariantSoldCount;
-import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantId;
-import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantProductId;
+import vn.edu.uit.msshop.product.variant.domain.model.creation.NewVariantSoldCount;
 
 public interface InitializeVariantSoldCountPort {
     VariantSoldCount initialize(
-            final VariantId id,
-            final VariantProductId productId);
+            final NewVariantSoldCount newSoldCount);
 }
