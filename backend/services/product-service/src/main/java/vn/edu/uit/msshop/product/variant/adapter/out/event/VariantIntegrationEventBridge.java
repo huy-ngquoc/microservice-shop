7c1dab10130825f910e64d5589ab6a5aa17907fd
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-05-04 11:18:39 +0700

    Implement Kafka integration for variant events with producer and listener components.

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/out/event/VariantIntegrationEventBridge.java
@@ -0,0 +1,53 @@
+package vn.edu.uit.msshop.product.variant.adapter.out.event;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.variant.application.dto.integration.VariantSoftDeletedIntegrationEvent;
+import vn.edu.uit.msshop.product.variant.application.dto.integration.VariantUpdatedIntegrationEvent;
+import vn.edu.uit.msshop.product.variant.application.exception.VariantNotFoundException;
+import vn.edu.uit.msshop.product.variant.application.port.out.event.PublishVariantIntegrationEventPort;
+import vn.edu.uit.msshop.product.variant.application.port.out.persistence.LoadVariantPort;
+import vn.edu.uit.msshop.product.variant.domain.event.VariantSoftDeleted;
+import vn.edu.uit.msshop.product.variant.domain.event.VariantUpdated;
+import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantImageKey;
+
+@Component
+@RequiredArgsConstructor
+public class VariantIntegrationEventBridge {
+    private final LoadVariantPort loadPort;
+    private final PublishVariantIntegrationEventPort integrationPort;
+
+    @TransactionalEventListener(
+            phase = TransactionPhase.AFTER_COMMIT)
+    public void on(
+            final VariantUpdated event) {
+        final var variantId = event.getVariantId();
+        final var variant = this.loadPort.loadById(variantId)
+                .orElseThrow(() -> new VariantNotFoundException(variantId));
+
+        final var msg = new VariantUpdatedIntegrationEvent(
+                UUID.randomUUID(),
+                variant.getId().value(),
+                variant.getTraits().unwrap(),
+                variant.getPrice().value(),
+                variant.getProductName().value(),
+                VariantImageKey.unwrap(variant.getImageKey()));
+
+        this.integrationPort.publishUpdated(msg);
+    }
+
+    @TransactionalEventListener(
+            phase = TransactionPhase.AFTER_COMMIT)
+    public void on(
+            final VariantSoftDeleted event) {
+        final var msg = new VariantSoftDeletedIntegrationEvent(
+                UUID.randomUUID(),
+                event.getVariantId().value());
+        this.integrationPort.publishSoftDeleted(msg);
+    }
+}
