c8caf537d291c4befc9e35c9fe249d35f94a216c
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-05-04 11:18:39 +0700

    Implement Kafka integration for variant events with producer and listener components.

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/bootstrap/config/MongoConfig.java
@@ -0,0 +1,16 @@
+package vn.edu.uit.msshop.product.bootstrap.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTransactionManager transactionManager(
+            MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+}
--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/out/sync/InventoryStockCountFeignClient.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/adapter/out/sync/InventoryStockCountFeignClient.java
@@ -7,6 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import vn.edu.uit.msshop.product.variant.adapter.out.sync.response.StockCountResponse;
 
+// @PostMapping("/public/updated_inventory")
+//     public ResponseEntity<Page<InventoryResponse>> getUpdatedInventory(@RequestBody GetUpdatedInventoryRequest request, @RequestParam(defaultValue="0") int pageNumber, @RequestParam(defaultValue="7") int pageSize) {
+//         Pageable pageable = PageRequest.of(pageNumber, pageSize);
+//         final var result = findUseCase.findAllUpdatedInventory(request.getStartFirst(), request.getEndFirst(), request.getStartSecond(), request.getEndSecond(), pageable);
+//         return ResponseEntity.ok(result.map(mapper::toResponse));
+//     }
+
 @FeignClient(
         name = "inventory-service")
 public interface InventoryStockCountFeignClient {
