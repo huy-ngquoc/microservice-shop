b02029a62bcd60441974a06cdcfdaeb915477e4a
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-05-11 15:16:18 +0700

    refactor(payment): migrate to UUID v7 generation

--- backend/services/payment-service/payment/src/main/java/vn/uit/edu/payment/adapter/in/web/OrderEventListener.java
+++ backend/services/payment-service/payment/src/main/java/vn/uit/edu/payment/adapter/in/web/OrderEventListener.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import vn.edu.uit.msshop.shared.domain.identifier.UUIDs;
 import vn.uit.edu.payment.adapter.in.web.mapper.PaymentWebMapper;
 import vn.uit.edu.payment.adapter.out.event.documents.EventDocument;
 import vn.uit.edu.payment.adapter.out.event.documents.PaymentCreatedFailDocument;
@@ -30,15 +31,15 @@ import vn.uit.edu.payment.domain.model.Payment;
 import vn.uit.edu.payment.domain.model.valueobject.OrderId;
 import vn.uit.edu.payment.domain.model.valueobject.PaymentStatus;
 
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
 @RetryableTopic(
-    attempts = "4", 
-    include = {RuntimeException.class} 
-)
-@KafkaListener(topics="order-topic",groupId="order-payment-group")
+        attempts = "4",
+        include = { RuntimeException.class })
+@KafkaListener(
+        topics = "order-topic",
+        groupId = "order-payment-group")
 public class OrderEventListener {
     private final PaymentWebMapper mapper;
     private final CreatePaymentUseCase createUseCase;
@@ -50,92 +51,106 @@ public class OrderEventListener {
     private final PaymentCreatedFailedRepository paymentCreatedFailRepo;
     private final PublishPaymentEventPort publishEventPort;
     private final PaymentCreatedFailService paymentCreatedFailService;
-    
 
     @KafkaHandler
-    
-    public void handleOrderCreated(OrderCreated event) {
-        //Random rand = new Random();
-        //if(rand.nextLong(200)<=20) throw new RuntimeException("Runtime exception");
-        //System.out.println("Listen to event");
-        if(!eventDocumentRepo.existsById(event.eventId())) {
-        CreatePaymentCommand command = mapper.toCommand(event);
-        createUseCase.create(command);
-        eventDocumentRepo.save(new EventDocument(event.eventId(), Instant.now()));
+
+    public void handleOrderCreated(
+            OrderCreated event) {
+        // Random rand = new Random();
+        // if(rand.nextLong(200)<=20) throw new RuntimeException("Runtime exception");
+        // System.out.println("Listen to event");
+        if (!eventDocumentRepo.existsById(event.eventId())) {
+            CreatePaymentCommand command = mapper.toCommand(event);
+            createUseCase.create(command);
+            eventDocumentRepo.save(new EventDocument(event.eventId(), Instant.now()));
         }
     }
-    
+
     @DltHandler
-    public void paymentCreatedFail(OrderCreated event) {
+    public void paymentCreatedFail(
+            OrderCreated event) {
         PaymentCreatedFailDocument document = PaymentCreatedFailDocument.builder()
-        .eventId(UUID.randomUUID())
-        .orderId(event.orderId())
-        .userId(event.userId())
-        .userEmail(event.userEmail())
-        .retryCount(0)
-        .createdAt(Instant.now())
-        .updatedAt(null)
-        .lastError(null).build();
+                .eventId(UUIDs.newId())
+                .orderId(event.orderId())
+                .userId(event.userId())
+                .userEmail(event.userEmail())
+                .retryCount(0)
+                .createdAt(Instant.now())
+                .updatedAt(null)
+                .lastError(null).build();
         paymentCreatedFailService.saveAndSendPaymentCreatedFail(document);
 
     }
+
     @KafkaHandler
     @Transactional
-    public void handleOrderUpdated(OrderUpdatedEvent event) {
-        if(!eventDocumentRepo.existsById(event.getEventId())) {
+    public void handleOrderUpdated(
+            OrderUpdatedEvent event) {
+        if (!eventDocumentRepo.existsById(event.getEventId())) {
 
-            if(event.getPaymentMethod().equals("COD")) {
-                /*if(event.getStatus().equals("CANCELLED")) {
-                    handleCodOrderCancelled(event.getOrderId());
-                }
-                if(event.getStatus().equals("RECEIVED")){
-                    handleCodOrderReceived(event.getOrderId());
-                }*/
-               return;
-            }
-            else {
-                
-                if(event.getStatus().equals("CANCELLED")) {
+            if (event.getPaymentMethod().equals("COD")) {
+                /*
+                 * if(event.getStatus().equals("CANCELLED")) {
+                 * handleCodOrderCancelled(event.getOrderId());
+                 * }
+                 * if(event.getStatus().equals("RECEIVED")){
+                 * handleCodOrderReceived(event.getOrderId());
+                 * }
+                 */
+                return;
+            } else {
+
+                if (event.getStatus().equals("CANCELLED")) {
                     handleOnlinePaymentCancelled(event.getOrderId());
                 }
-                
+
             }
         }
     }
-    @KafkaHandler(isDefault=true) 
-    public void onObjectReceived(Object event) {
+
+    @KafkaHandler(
+            isDefault = true)
+    public void onObjectReceived(
+            Object event) {
         System.out.println("Nhan event la");
-        
+
     }
-    private void handleOnlinePaymentCancelled(UUID orderId) {
+
+    private void handleOnlinePaymentCancelled(
+            UUID orderId) {
         Payment payment = loadPort.loadPaymentByOrderId(new OrderId(orderId));
-        if(payment!=null) {
-            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId()).currency(payment.getCurrency())
-            .paymentStatus(new PaymentStatus("CANCELLED")).paymentMethod(payment.getPaymentMethod()).build();
+        if (payment != null) {
+            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId())
+                    .currency(payment.getCurrency())
+                    .paymentStatus(new PaymentStatus("CANCELLED")).paymentMethod(payment.getPaymentMethod()).build();
             final var saved = payment.applyUpdateInfo(updateInfo);
             savePort.save(saved);
             cancelPaymentLinkPort.cancelPaymentLink(saved.getOrderId());
         }
     }
-    private void handleCodOrderCancelled(UUID orderId) {
+
+    private void handleCodOrderCancelled(
+            UUID orderId) {
         Payment payment = loadPort.loadPaymentByOrderId(new OrderId(orderId));
-        if(payment!=null) {
-            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId()).currency(payment.getCurrency())
-            .paymentStatus(new PaymentStatus("CANCELLED")).paymentMethod(payment.getPaymentMethod()).build();
+        if (payment != null) {
+            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId())
+                    .currency(payment.getCurrency())
+                    .paymentStatus(new PaymentStatus("CANCELLED")).paymentMethod(payment.getPaymentMethod()).build();
             final var saved = payment.applyUpdateInfo(updateInfo);
             savePort.save(saved);
         }
     }
-    private void handleCodOrderReceived(UUID orderId) {
+
+    private void handleCodOrderReceived(
+            UUID orderId) {
         Payment payment = loadPort.loadPaymentByOrderId(new OrderId(orderId));
-        if(payment!=null) {
-            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId()).currency(payment.getCurrency())
-            .paymentStatus(new PaymentStatus("SUCCESS")).paymentMethod(payment.getPaymentMethod()).build();
+        if (payment != null) {
+            final var updateInfo = Payment.UpdateInfo.builder().paymentId(payment.getPaymentId())
+                    .currency(payment.getCurrency())
+                    .paymentStatus(new PaymentStatus("SUCCESS")).paymentMethod(payment.getPaymentMethod()).build();
             final var saved = payment.applyUpdateInfo(updateInfo);
             savePort.save(saved);
         }
     }
-   
-    
 
 }
--- backend/services/payment-service/payment/src/main/java/vn/uit/edu/payment/adapter/in/web/mapper/PaymentWebMapper.java
+++ backend/services/payment-service/payment/src/main/java/vn/uit/edu/payment/adapter/in/web/mapper/PaymentWebMapper.java
@@ -1,9 +1,8 @@
 package vn.uit.edu.payment.adapter.in.web.mapper;
 
-import java.util.UUID;
-
 import org.springframework.stereotype.Component;
 
+import vn.edu.uit.msshop.shared.domain.identifier.UUIDs;
 import vn.uit.edu.payment.adapter.in.web.request.CreatePaymentRequest;
 import vn.uit.edu.payment.adapter.in.web.request.UpdatePaymentRequest;
 import vn.uit.edu.payment.adapter.in.web.request.common.ChangeRequest;
@@ -20,6 +19,7 @@ import vn.uit.edu.payment.domain.model.valueobject.PaymentStatus;
 import vn.uit.edu.payment.domain.model.valueobject.PaymentValue;
 import vn.uit.edu.payment.domain.model.valueobject.UserEmail;
 import vn.uit.edu.payment.domain.model.valueobject.UserId;
+
 /*PaymentId paymentId,
     Currency currency,
     OrderId orderId,
@@ -28,41 +28,45 @@ import vn.uit.edu.payment.domain.model.valueobject.UserId;
     PaymentValue paymentValue */
 @Component
 public class PaymentWebMapper {
-    public CreatePaymentCommand toCommand(CreatePaymentRequest request) {
+    public CreatePaymentCommand toCommand(
+            CreatePaymentRequest request) {
         return new CreatePaymentCommand(
-            new PaymentId(UUID.randomUUID()),
-            new Currency(request.currency()),
-            new OrderId(request.orderId()),
-            new PaymentMethod(request.paymentMethod()),
-            new PaymentStatus("PENDING"),
-            new PaymentValue(request.paymentValue()),
-            new UserId(request.userId()),
-            new UserEmail(request.userEmail())
-        );
+                new PaymentId(UUIDs.newId()),
+                new Currency(request.currency()),
+                new OrderId(request.orderId()),
+                new PaymentMethod(request.paymentMethod()),
+                new PaymentStatus("PENDING"),
+                new PaymentValue(request.paymentValue()),
+                new UserId(request.userId()),
+                new UserEmail(request.userEmail()));
     }
 
-    public CreatePaymentCommand toCommand(OrderCreated event) {
+    public CreatePaymentCommand toCommand(
+            OrderCreated event) {
         return new CreatePaymentCommand(
-            new PaymentId(UUID.randomUUID()),
-            new Currency(event.currency()),
-            new OrderId(event.orderId()),
-            new PaymentMethod(event.paymentMethod()),
-            new PaymentStatus("PENDING"),
-            new PaymentValue(event.paymentValue()),
-        new UserId(event.userId()),new UserEmail(event.userEmail())
-        );
+                new PaymentId(UUIDs.newId()),
+                new Currency(event.currency()),
+                new OrderId(event.orderId()),
+                new PaymentMethod(event.paymentMethod()),
+                new PaymentStatus("PENDING"),
+                new PaymentValue(event.paymentValue()),
+                new UserId(event.userId()), new UserEmail(event.userEmail()));
     }
-    
-    public UpdatePaymentCommand toCommand(UpdatePaymentRequest request) {
+
+    public UpdatePaymentCommand toCommand(
+            UpdatePaymentRequest request) {
         final var paymentId = new PaymentId(request.paymentId());
         final var currency = ChangeRequest.toChange(request.currency(), Currency::new);
-        final var paymentMethod = ChangeRequest.toChange(request.paymentMethod(),PaymentMethod::new);
+        final var paymentMethod = ChangeRequest.toChange(request.paymentMethod(), PaymentMethod::new);
         final var paymentStatus = ChangeRequest.toChange(request.paymentStatus(), PaymentStatus::new);
         return new UpdatePaymentCommand(paymentId, currency, paymentStatus, paymentMethod);
     }
 
-    public PaymentResponse toResponse(PaymentView paymentView) {
-        return new PaymentResponse(paymentView.paymentId(), paymentView.createAt(), paymentView.currency(), paymentView.orderId(), paymentView.paymentMethod(), paymentView.paymentStatus(), paymentView.paymentValue(), paymentView.updateAt(), paymentView.userId());
+    public PaymentResponse toResponse(
+            PaymentView paymentView) {
+        return new PaymentResponse(paymentView.paymentId(), paymentView.createAt(), paymentView.currency(),
+                paymentView.orderId(), paymentView.paymentMethod(), paymentView.paymentStatus(),
+                paymentView.paymentValue(), paymentView.updateAt(), paymentView.userId());
     }
 
 }
