ea2b2e789674cfc69479ca9449e6377278a82e29
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-05-23 11:39:03 +0700

    feat(rating-service): publish rating sync events to kafka

--- backend/services/rating-service/rating/src/main/java/vn/uit/edu/msshop/rating/application/service/DeleteRatingService.java
+++ backend/services/rating-service/rating/src/main/java/vn/uit/edu/msshop/rating/application/service/DeleteRatingService.java
@@ -1,10 +1,12 @@
 package vn.uit.edu.msshop.rating.application.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 import vn.uit.edu.msshop.rating.application.port.in.DeleteRatingUseCase;
 import vn.uit.edu.msshop.rating.application.port.out.DeleteRatingPort;
+import vn.uit.edu.msshop.rating.application.port.out.LoadRatingPort;
 import vn.uit.edu.msshop.rating.application.port.out.PublishRatingEvent;
 import vn.uit.edu.msshop.rating.domain.event.RatingDeleted;
 import vn.uit.edu.msshop.rating.domain.model.valueobject.RatingId;
@@ -12,14 +14,23 @@ import vn.uit.edu.msshop.rating.domain.model.valueobject.RatingId;
 @Service
 @RequiredArgsConstructor
 public class DeleteRatingService implements DeleteRatingUseCase {
+    private final LoadRatingPort loadPort;
     private final DeleteRatingPort deletePort;
     private final PublishRatingEvent eventPublisher;
 
     @Override
+    @Transactional
     public void delete(
             RatingId id) {
+        final var rating = this.loadPort.loadById(id);
+
         deletePort.deleteById(id);
-        eventPublisher.publish(new RatingDeleted(id));
+
+        final var event = new RatingDeleted(
+                rating.getId(),
+                rating.getProductId(),
+                rating.getRatingPoint());
+        eventPublisher.publish(event);
     }
 
 }
