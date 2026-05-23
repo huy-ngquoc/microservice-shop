67d9ee0fb2c4f09cce5c5dc4a512df586084ebfb
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-05-23 11:39:21 +0700

    feat(product-service): consume rating sync events to update ProductRating

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/adapter/in/event/ProductRatingEventListener.java
@@ -0,0 +1,54 @@
+package vn.edu.uit.msshop.product.product.adapter.in.event;
+
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import vn.edu.uit.msshop.product.product.adapter.in.event.payload.RatingCreatedIntegrationEvent;
+import vn.edu.uit.msshop.product.product.adapter.in.event.payload.RatingDeletedIntegrationEvent;
+import vn.edu.uit.msshop.product.product.adapter.in.event.payload.RatingUpdatedIntegrationEvent;
+import vn.edu.uit.msshop.product.product.application.port.in.command.ApplyRatingCreatedUseCase;
+import vn.edu.uit.msshop.product.product.application.port.in.command.ApplyRatingDeletedUseCase;
+import vn.edu.uit.msshop.product.product.application.port.in.command.ApplyRatingUpdatedUseCase;
+import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductId;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+@KafkaListener(
+        topics = "rating-product")
+public class ProductRatingEventListener {
+    private final ApplyRatingCreatedUseCase createdUseCase;
+    private final ApplyRatingUpdatedUseCase updatedUseCase;
+    private final ApplyRatingDeletedUseCase deletedUseCase;
+
+    @KafkaHandler
+    public void onCreated(
+            final RatingCreatedIntegrationEvent event) {
+        this.createdUseCase.execute(
+                event.eventId(),
+                new ProductId(event.productId()),
+                event.point());
+    }
+
+    @KafkaHandler
+    public void onUpdate(
+            final RatingUpdatedIntegrationEvent event) {
+        this.updatedUseCase.execute(
+                event.eventId(),
+                new ProductId(event.productId()),
+                event.oldPoint(),
+                event.newPoint());
+    }
+
+    @KafkaHandler
+    public void onDeleted(
+            final RatingDeletedIntegrationEvent event) {
+        this.deletedUseCase.execute(
+                event.eventId(),
+                new ProductId(event.productId()),
+                event.point());
+    }
+}
