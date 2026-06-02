e44562a4457dfdb4b091c07bfc03c4af78512508
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-02 17:17:09 +0700

    refactor(product-service): group category port/in/queries into nested lookup use case wrappers.

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/application/port/in/query/CheckCategoryExistsUseCase.java
+++
@@ -1,8 +0,0 @@
-package vn.edu.uit.msshop.product.category.application.port.in.query;
-
-import vn.edu.uit.msshop.product.category.domain.model.valueobject.CategoryId;
-
-public interface CheckCategoryExistsUseCase {
-    boolean existsById(
-            final CategoryId id);
-}
