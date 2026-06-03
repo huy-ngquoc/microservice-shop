d1d26fdc2e3319c5c24ebc380d2126bfce69d69e
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-03 14:40:44 +0700

    refactor(product-service): group product command services into concept packages

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/application/service/command/count/IncreaseProductSoldCountsForVariantsService.java
@@ -0,0 +1,35 @@
+package vn.edu.uit.msshop.product.product.application.service.command.count;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.bootstrap.config.cache.CacheNames;
+import vn.edu.uit.msshop.product.product.application.dto.command.IncreaseProductSoldCountsForVariantsCommand;
+import vn.edu.uit.msshop.product.product.application.port.in.command.IncreaseProductSoldCountsForVariantsUseCase;
+import vn.edu.uit.msshop.product.product.application.port.out.persistence.IncreaseAllProductSoldCountsPort;
+
+@Service
+@RequiredArgsConstructor
+public class IncreaseProductSoldCountsForVariantsService
+        implements IncreaseProductSoldCountsForVariantsUseCase {
+    private final IncreaseAllProductSoldCountsPort increaseAllSoldCountsPort;
+
+    @Override
+    @Transactional
+    @Caching(
+            evict = {
+                    @CacheEvict(
+                            cacheNames = CacheNames.PRODUCT,
+                            allEntries = true),
+                    @CacheEvict(
+                            cacheNames = CacheNames.PRODUCT_LIST,
+                            allEntries = true)
+            })
+    public void execute(
+            final IncreaseProductSoldCountsForVariantsCommand command) {
+        this.increaseAllSoldCountsPort.increaseAll(command.incrementById());
+    }
+}
