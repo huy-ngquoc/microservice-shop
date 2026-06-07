71a0dab655cbe44933cff416b4f8046bd0a16599
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-07 20:39:22 +0700

    refactor(product-service): group category query services into operation packages

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/application/service/query/lookup/CategorySoftDeletedLookupByIdService.java
@@ -0,0 +1,31 @@
+package vn.edu.uit.msshop.product.category.application.service.query.lookup;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.category.application.dto.view.CategoryView;
+import vn.edu.uit.msshop.product.category.application.exception.CategoryNotFoundException;
+import vn.edu.uit.msshop.product.category.application.mapper.CategoryViewMapper;
+import vn.edu.uit.msshop.product.category.application.port.in.query.CategoryLookupUseCases;
+import vn.edu.uit.msshop.product.category.application.port.out.persistence.LoadSoftDeletedCategoryPort;
+import vn.edu.uit.msshop.product.category.domain.model.valueobject.CategoryId;
+
+@Service
+@RequiredArgsConstructor
+public class CategorySoftDeletedLookupByIdService
+        implements CategoryLookupUseCases.FindSoftDeletedById {
+
+    private final LoadSoftDeletedCategoryPort loadSoftDeletedPort;
+    private final CategoryViewMapper mapper;
+
+    @Override
+    @Transactional(
+            readOnly = true)
+    public CategoryView findSoftDeletedById(
+            final CategoryId id) {
+        return this.loadSoftDeletedPort.loadSoftDeletedById(id)
+                .map(this.mapper::toView)
+                .orElseThrow(() -> new CategoryNotFoundException(id));
+    }
+}
