1a788d017128c257f4c22a430d9555cd9335e625
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-07 20:39:22 +0700

    refactor(product-service): group category query services into operation packages

---
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/application/service/query/existence/CategoryActiveExistenceCheckByIdService.java
@@ -0,0 +1,25 @@
+package vn.edu.uit.msshop.product.category.application.service.query.existence;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import vn.edu.uit.msshop.product.category.application.port.in.query.CategoryLookupUseCases;
+import vn.edu.uit.msshop.product.category.application.port.out.persistence.CheckCategoryExistsPort;
+import vn.edu.uit.msshop.product.category.domain.model.valueobject.CategoryId;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryActiveExistenceCheckByIdService
+        implements CategoryLookupUseCases.CheckExistsById {
+
+    private final CheckCategoryExistsPort checkExistsPort;
+
+    @Override
+    @Transactional(
+            readOnly = true)
+    public boolean existsById(
+            final CategoryId id) {
+        return this.checkExistsPort.existsById(id);
+    }
+}
