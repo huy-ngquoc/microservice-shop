ea86983e6fe9140749477f949b0897d8804affe9
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-11 17:36:21 +0700

    refactor(product-service): rename processed rating event collection to Product* convention

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/adapter/out/persistence/ratingevent/ProductProcessedRatingEventDocument.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/adapter/out/persistence/ratingevent/ProductProcessedRatingEventDocument.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
-@Document("ProcessedRatingEvents")
+@Document("ProductProcessedRatingEvents")
 @Getter
 @EqualsAndHashCode(
         onlyExplicitlyIncluded = true)
