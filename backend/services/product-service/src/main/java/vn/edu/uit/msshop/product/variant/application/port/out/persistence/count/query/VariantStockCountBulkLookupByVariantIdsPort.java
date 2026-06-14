f991ec91b4637fcc21bfdf2074ee8fa474710565
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-15 00:03:03 +0700

    refactor(product-service): align variant stock count bulk-lookup param to Set<VariantId>

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/application/port/out/persistence/count/query/VariantStockCountBulkLookupByVariantIdsPort.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/variant/application/port/out/persistence/count/query/VariantStockCountBulkLookupByVariantIdsPort.java
@@ -1,12 +1,12 @@
 package vn.edu.uit.msshop.product.variant.application.port.out.persistence.count.query;
 
-import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 import vn.edu.uit.msshop.product.variant.domain.model.VariantStockCount;
 import vn.edu.uit.msshop.product.variant.domain.model.valueobject.VariantId;
 
 public interface VariantStockCountBulkLookupByVariantIdsPort {
     Map<VariantId, VariantStockCount> loadAllByVariantIds(
-            final Collection<VariantId> variantIdCollection);
+            final Set<VariantId> variantIdSet);
 }
