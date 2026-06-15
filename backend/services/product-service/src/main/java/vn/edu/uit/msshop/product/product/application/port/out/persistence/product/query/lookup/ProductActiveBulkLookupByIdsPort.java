6d3616c0ca89dc43ff70324cfe24cc99f68c766e
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-15 17:34:47 +0700

    refactor(product-service): return map keyed by id from ProductActiveBulkLookupByIdsPort

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/application/port/out/persistence/product/query/lookup/ProductActiveBulkLookupByIdsPort.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/product/application/port/out/persistence/product/query/lookup/ProductActiveBulkLookupByIdsPort.java
@@ -1,12 +1,12 @@
 package vn.edu.uit.msshop.product.product.application.port.out.persistence.product.query.lookup;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import vn.edu.uit.msshop.product.product.domain.model.Product;
 import vn.edu.uit.msshop.product.product.domain.model.valueobject.ProductId;
 
 public interface ProductActiveBulkLookupByIdsPort {
-    List<Product> loadAllByIds(
-            final Collection<ProductId> ids);
+    Map<ProductId, Product> loadAllByIds(
+            final Set<ProductId> idSet);
 }
