3f2d261409e313f246f5175a18f3fdc4b30484af
huy-ngquoc <22520565@gm.uit.edu.vn>
2026-06-18 21:50:46 +0700

    fix(product-service): repair cache key/condition SpEL expressions

--- backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/application/service/command/lifecycle/CategorySoftDeletionByIdService.java
+++ backend/services/product-service/src/main/java/vn/edu/uit/msshop/product/category/application/service/command/lifecycle/CategorySoftDeletionByIdService.java
@@ -38,7 +38,7 @@ class CategorySoftDeletionByIdService
             evict = {
                     @CacheEvict(
                             cacheNames = CacheNames.CATEGORY,
-                            key = "#cmd.id().value()"),
+                            key = "#cmd.categoryId()"),
                     @CacheEvict(
                             cacheNames = CacheNames.CATEGORY_LIST,
                             allEntries = true)
